a4a0d0ba812dc01426fb384aed5d9d9f894279a8
sryokemura <lymarques@inf.ufpel.edu.br>
2016-04-13 17:27:08 -0300

    Added files via upload
    Versão 2.0

--- solucionavel.py
+++ solucionavel.py
@@ -1,20 +1,24 @@
-estadoinicial = [1,5,8,0,2,3,4,6,7]
-print (estadoinicial[1])
-print (estadoinicial[0])
-print (estadoinicial[5])
-count = 0
-for x in range(9):
-    print ("Esse: ")       
-    print (estadoinicial[x])
-    if(estadoinicial[x] != 0):
-        for y in range((x+1), 9):
-            if(estadoinicial[y] != 0):
-                print ("Compara com esse: ")
-                print (estadoinicial[y])
-                if(estadoinicial[x] > estadoinicial[y]):
-                    count = count + 1
-print ("Numero total de inversoes: ", count)
-if((count % 2) == 0):
-    print ("Soluvel")
+def soluvel(vetor): #vetor de entrada
+    saida = 0
+    for x in range(9):
+        if(vetor[x] != 0):
+            for y in range((x+1), 9):
+                if(vetor[y] != 0):
+                    if(vetor[x] > vetor[y]):
+                        saida = saida + 1
+    print ("Numero total de inversoes: ", saida)
+    return saida #retorna numero de inversoes, fazer saida%2 == 0 pra saber se eh soluvel
 
-#Tranformar saporra em funçao
+estadoinicial = [1,5,8,0,2,3,4,6,7]
+teste2 = [5,1,8,0,2,3,4,6,7]
+teste3 = [2,0,7,8,5,4,3,6,1]
+teste4 = [8,7,6,5,4,3,2,1,0]
+teste = 0
+teste = soluvel(estadoinicial)
+print(teste)
+teste = soluvel(teste2)
+print(teste)
+teste = soluvel(teste3)
+print(teste)
+teste = soluvel(teste4)
+print(teste)
