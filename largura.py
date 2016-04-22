1d19617015aff67ab950db19f7fe95ce3acce45d
lucaslima94 <lcdsdlima@inf.ufpel.edu.br>
2016-04-22 14:44:55 -0300

    busca em largura

--- largura.py
+++ largura.py
@@ -1,204 +1,193 @@
 def teste(vet, vet2):
         valor = 0
         for i in range(0,(len(vet))):
-                if(vet[i] == vet2):
-                        valor = valor + 1
+			if(vet[i] == vet2):
+				valor = valor + 1
         return valor
+        
+def buscameta(inicial,meta):
+	valor=0
+	if (inicial==meta):
+		valor=1
+	return valor
 
-class NO(object):
-	valor=None
-	global listanodos
-	def init(self,matriz):
-		self.valor=matriz
-	def gera_filhos(self,visitados):
-		for x in range(0,9):
-			if self.valor[x]==0:
-				pos=x
-		if pos==0:
-			matriz1=self.valor[:]
-			matriz1[0],matriz1[1]=matriz1[1],matriz1[0]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			matriz1=self.valor[:]
-			matriz1[0],matriz1[3]=matriz1[3],matriz1[0]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			listanodos.append(None)
-			listanodos.append(None)
+def gerafilhos(vetor,listanodos):
+	vetorfilhos = []
+	temp = vetor[:]
+	temp1 = vetor[:]
+	temp2 = vetor[:]
+	temp3 = vetor[:]
+	for x in range(0,9):
+		if vetor[x]==0:
+			pos=x
+	if pos == 0:
+		temp[0],temp[1]=temp[1],temp[0]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[0],temp1[3]=temp1[3],temp1[0]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 2:
+		temp[2],temp[1]=temp[1],temp[2]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[2],temp1[5]=temp1[5],temp1[2]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 6:
+		temp[6],temp[3]=temp[3],temp[6]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[6],temp1[7]=temp1[7],temp1[6]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 8:
+		temp[8],temp[7]=temp[7],temp[8]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[8],temp1[5]=temp1[5],temp1[8]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 1:
+		temp[1],temp[0]=temp[0],temp[1]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[1],temp1[4]=temp1[4],temp1[1]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[1],temp2[2]=temp2[2],temp2[1]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 3:
+		temp[3],temp[0]=temp[0],temp[3]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[3],temp1[4]=temp1[4],temp1[3]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[3],temp2[6]=temp2[6],temp2[3]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 5:
+		temp[5],temp[2]=temp[2],temp[5]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[5],temp1[4]=temp1[4],temp1[5]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[5],temp2[8]=temp2[8],temp2[5]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 7:
+		temp[7],temp[6]=temp[6],temp[7]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[7],temp1[4]=temp1[4],temp1[7]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[7],temp2[8]=temp2[8],temp2[7]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 4:
+		temp[4],temp[1]=temp[1],temp[4]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[4],temp1[3]=temp1[3],temp1[4]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[4],temp2[5]=temp2[5],temp2[4]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		temp3[4],temp3[7]=temp3[7],temp3[4]
+		if(teste(listanodos,temp3)==0):
+			vetorfilhos.append(temp3)
+		else:
+			vetorfilhos.append(None)
+	return vetorfilhos
+			
 		
-		if pos==2:
-			matriz1=self.valor[:]
-			matriz1[2],matriz1[1]=matriz1[1],matriz1[2]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			matriz1=self.valor[:]
-			matriz1[2],matriz1[5]=matriz1[5],matriz1[2]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			listanodos.append(None)
-			listanodos.append(None)
-			
-		if pos==6:
-			matriz1=self.valor[:]
-			matriz1[6],matriz1[3]=matriz1[3],matriz1[6]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			matriz1=self.valor[:]
-			matriz1[6],matriz1[7]=matriz1[7],matriz1[3]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			listanodos.append(None)
-			listanodos.append(None)
-			
-		if pos==8:
-			matriz1=self.valor[:]
-			matriz1[8],matriz1[7]=matriz1[7],matriz1[8]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			matriz1=self.valor[:]
-			matriz1[8],matriz1[5]=matriz1[5],matriz1[8]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			listanodos.append(None)
-			listanodos.append(None)
-			
-		if pos==1:
-			matriz1=self.valor[:]
-			matriz1[1],matriz1[0]=matriz1[0],matriz1[1]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			matriz1=self.valor[:]
-			matriz1[1],matriz1[4]=matriz1[4],matriz1[1]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			matriz1=self.valor[:]
-			matriz1[1],matriz1[2]=matriz1[2],matriz1[1]
-			if(teste(visitados, matriz1) == 0):
-				temporario=NO()
-				temporario.init(matriz1)
-				listanodos.append(temporario)
-			listanodos.append(None)
-			
-			if pos==3:
-				matriz1=self.valor[:]
-				matriz1[3],matriz1[0]=matriz1[0],matriz1[3]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[3],matriz1[4]=matriz1[4],matriz1[3]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[3],matriz1[6]=matriz1[6],matriz1[3]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				listanodos.append(None)
-			
-			if pos==5:
-				matriz1=self.valor[:]
-				matriz1[5],matriz1[2]=matriz1[2],matriz1[5]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[5],matriz1[4]=matriz1[4],matriz1[5]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[5],matriz1[8]=matriz1[8],matriz1[5]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				listanodos.append(None)
-			
-			if pos==7:
-				matriz1=self.valor[:]
-				matriz1[7],matriz1[6]=matriz1[6],matriz1[7]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[7],matriz1[4]=matriz1[4],matriz1[7]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[7],matriz1[8]=matriz1[8],matriz1[7]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				listanodos.append(None)
-			
-			if pos==4:
-				matriz1=self.valor[:]
-				matriz1[4],matriz1[1]=matriz1[1],matriz1[4]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[4],matriz1[3]=matriz1[3],matriz1[4]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[4],matriz1[5]=matriz1[5],matriz1[4]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
-				matriz1=self.valor[:]
-				matriz1[4],matriz1[7]=matriz1[7],matriz1[4]
-				if(teste(visitados, matriz1) == 0):
-					temporario=NO()
-					temporario.init(matriz1)
-					listanodos.append(temporario)
 		
-estadoinicial=[0,1,2,3,4,5,6,7,8]
-raiz=NO()
-raiz.init(estadoinicial)
-atual=raiz
+estadofinal=[0,1,2,3,4,5,6,7,8]
+estadoinicial=[3,1,2,6,4,5,0,7,8]
+estadoinicial2=[1,5,8,0,2,3,4,6,7]
+estadoinicial3=[1,4,2,3,7,5,6,0,8]
+
 listanodos=[]
-for j in range(0,10):
-	listanodos.append(atual)
-	atual.gera_filhos(listanodos)
-for i in range(0,len(listanodos)):
-	if (listanodos[i]!= None):
-		print listanodos[i].valor
+listatemp = []
+contador=0
+listanodos.append(estadoinicial3)
+while(listanodos[contador] != estadofinal):
+	print listanodos[contador]
+	atual = listanodos[contador]
+	if(listanodos[contador] == None):
+		listanodos.append(None)
+		listanodos.append(None)
+		listanodos.append(None)
+		listanodos.append(None)
 	else:
-		print "Nulo"
-print len(listanodos)
+		listatemp = gerafilhos(atual,listanodos)
+		listanodos.append(listatemp[0])
+		listanodos.append(listatemp[1])
+		listanodos.append(listatemp[2])
+		listanodos.append(listatemp[3])
+	contador = contador + 1
+print listanodos[contador]
+	
