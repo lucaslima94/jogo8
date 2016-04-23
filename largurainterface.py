6625484f64294a26db84c514d5983dfcb2c331d5
vagnersampaio92 <vdssampaio@inf.ufpel.edu.br>
2016-04-23 20:56:20 -0300

    final

---
+++ largurainterface.py
@@ -0,0 +1,296 @@
+import sys
+from tkinter import*
+global a
+a=0
+def imprimef():
+    global a
+    
+    if((a>=0)and(a<len(vetor)-1)):
+        a=a+1
+        imprime()
+def imprimeb():
+    global a
+    if((a>0)and(a<len(vetor))):
+        a=a-1
+        imprime()
+def imprimefinal():
+    global a
+    a=len(vetor)
+    print(a)
+    a=a-1
+    imprime()
+def imprimeinicio():
+    global a
+    print(a)
+    a=0
+    imprime()
+    
+def imprime():
+    if(a<len(vetor)):
+        mlabel=Label(text=vetor[a][0],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel.place(x=205,y=150)
+        mlabel2=Label(text=vetor[a][1],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel2.place(x=255,y=150)
+        mlabel2=Label(text=vetor[a][2],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel2.place(x=305,y=150)
+        mlabel=Label(text=vetor[a][3],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel.place(x=205,y=220)
+        mlabel2=Label(text=vetor[a][4],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel2.place(x=255,y=220)
+        mlabel2=Label(text=vetor[a][5],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel2.place(x=305,y=220)
+        mlabel=Label(text=vetor[a][6],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel.place(x=205,y=290)
+        mlabel2=Label(text=vetor[a][7],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel2.place(x=255,y=290)
+        mlabel2=Label(text=vetor[a][8],fg='black',bg='white',font="Arial 24 bold",border=5)
+        mlabel2.place(x=305,y=290)
+
+def cria(vetor,nodo,caminho):
+    mGui = Tk()
+    mGui.geometry('520x420')
+    mGui.title('jogo do 8')
+    
+    mlabel2=Label(text="caminhos:",fg='black',bg='white',font="Arial 15 bold",border=5)
+    mlabel2.place(x=75,y=350)
+    mlabel2=Label(text=caminho,fg='black',bg='white',font="Arial 15 bold",border=5)
+    mlabel2.place(x=180,y=350)
+    mlabel2=Label(text="nodos:",fg='black',bg='white',font="Arial 15 bold",border=5)
+    mlabel2.place(x=255,y=350)
+    mlabel2=Label(text=nodo,fg='black',bg='white',font="Arial 15 bold",border=5)
+    mlabel2.place(x=340,y=350)
+    b = Button(mGui, text="next", width=18, height=2,command=imprimef)
+    b.place(x=350,y=10)
+    b2 = Button(mGui, text="back", width=18, height=2,command=imprimeb)
+    b2.place(x=200,y=10)
+    b3 = Button(mGui, text="final", width=18, height=2,command=imprimefinal)
+    b3.place(x=50,y=10)
+    b4 = Button(mGui, text="inicio", width=18, height=2,command=imprimeinicio)
+    b4.place(x=200,y=60)
+    
+def teste(vet, vet2):
+	valor = 0
+	for i in range(0,(len(vet))):
+		if(vet[i] == vet2):
+			valor = valor + 1
+	return valor
+        
+def buscameta(inicial,meta):
+	valor=0
+	if (inicial==meta):
+		valor=1
+	return valor
+	
+def soluvel(vetor): #vetor de entrada
+    saida = 0
+    for x in range(9):
+        if(vetor[x] != 0):
+            for y in range((x+1), 9):
+                if(vetor[y] != 0):
+                    if(vetor[x] > vetor[y]):
+                        saida = saida + 1
+    print ("Numero total de inversoes: ", saida)
+    return saida
+
+def gerafilhos(vetor,listanodos):
+	vetorfilhos = []
+	temp = vetor[:]
+	temp1 = vetor[:]
+	temp2 = vetor[:]
+	temp3 = vetor[:]
+	for x in range(0,9):
+		if vetor[x]==0:
+			pos=x
+	if pos == 0:
+		temp[0],temp[1]=temp[1],temp[0]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[0],temp1[3]=temp1[3],temp1[0]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 2:
+		temp[2],temp[1]=temp[1],temp[2]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[2],temp1[5]=temp1[5],temp1[2]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 6:
+		temp[6],temp[3]=temp[3],temp[6]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[6],temp1[7]=temp1[7],temp1[6]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 8:
+		temp[8],temp[7]=temp[7],temp[8]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[8],temp1[5]=temp1[5],temp1[8]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 1:
+		temp[1],temp[0]=temp[0],temp[1]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[1],temp1[4]=temp1[4],temp1[1]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[1],temp2[2]=temp2[2],temp2[1]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 3:
+		temp[3],temp[0]=temp[0],temp[3]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[3],temp1[4]=temp1[4],temp1[3]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[3],temp2[6]=temp2[6],temp2[3]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 5:
+		temp[5],temp[2]=temp[2],temp[5]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[5],temp1[4]=temp1[4],temp1[5]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[5],temp2[8]=temp2[8],temp2[5]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 7:
+		temp[7],temp[6]=temp[6],temp[7]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[7],temp1[4]=temp1[4],temp1[7]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[7],temp2[8]=temp2[8],temp2[7]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		vetorfilhos.append(None)
+	if pos == 4:
+		temp[4],temp[1]=temp[1],temp[4]
+		if(teste(listanodos,temp)==0):
+			vetorfilhos.append(temp)
+		else:
+			vetorfilhos.append(None)
+		temp1[4],temp1[3]=temp1[3],temp1[4]
+		if(teste(listanodos,temp1)==0):
+			vetorfilhos.append(temp1)
+		else:
+			vetorfilhos.append(None)
+		temp2[4],temp2[5]=temp2[5],temp2[4]
+		if(teste(listanodos,temp2)==0):
+			vetorfilhos.append(temp2)
+		else:
+			vetorfilhos.append(None)
+		temp3[4],temp3[7]=temp3[7],temp3[4]
+		if(teste(listanodos,temp3)==0):
+			vetorfilhos.append(temp3)
+		else:
+			vetorfilhos.append(None)
+	return vetorfilhos
+			
+		
+		
+estadofinal=[0,1,2,3,4,5,6,7,8]
+estadoinicial=[3,1,2,6,4,5,0,7,8]
+estadoinicial2=[1,5,8,0,2,3,4,6,7]
+estadoinicial3=[1,4,2,3,7,5,6,0,8]
+estadoinicial4=[2,0,7,8,5,4,3,6,1]
+estadoinicial5=[0,2,1,3,4,5,6,7,8]
+
+vava = soluvel(estadoinicial)
+if(vava%2==0):
+	listafinal = []
+	listanodos=[]
+	listatemp = []
+	contador=0
+	listanodos.append(estadoinicial)
+	while(listanodos[contador] != estadofinal):
+		atual = listanodos[contador]
+		if(listanodos[contador] == None):
+			listanodos.append(None)
+			listanodos.append(None)
+			listanodos.append(None)
+			listanodos.append(None)
+		else:
+			listatemp = gerafilhos(atual,listanodos)
+			listanodos.append(listatemp[0])
+			listanodos.append(listatemp[1])
+			listanodos.append(listatemp[2])
+			listanodos.append(listatemp[3])
+		contador = contador + 1
+	listafinal.append(listanodos[contador])
+	volta = int((contador-0.1)/4)
+	profundidade = 1
+	while (volta != 0):
+		listafinal.append(listanodos[volta])
+		volta = int((volta-0.1)/4)
+		profundidade = profundidade + 1
+	listafinal.append(listanodos[0])
+	tamanhocam = profundidade + 1
+	print ("FInal:", listafinal) #Lista Final, precisa inverter, tamanhocam = tamanho do caminho, contador = numero de nos visitados
+	print ("Contador de nos visitados",contador) 
+	print ("Profundidade encontrado: ",profundidade)
+else:
+	print ("Nao solucionavel")
+	
+listafinal.reverse()
+vetor=listafinal
+cria(vetor,contador,profundidade)
+
+	
